5f68b78be443fdf9a384cf15bbb6334a99bb1022
Dmytro <ys33ys33ys55@gmail.com>
2026-01-12 16:29:58 +0100

    Solution fixed2

--- app/main.py
+++ app/main.py
@@ -36,7 +36,7 @@ class Dog(Animal):
 
 
 def feed_animals(animals_list: list[Animal]) -> int:
-    food_points = 0
-    for animal in animals_list:
-        food_points += animal.feed()
-    return food_points
+    return sum(
+        animal.feed()
+        for animal in animals_list
+    )
